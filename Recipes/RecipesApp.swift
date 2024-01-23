8cdcdaf41acdc99c714286ab8b44a1993ca92be7
Angel Colon-Ramirez <angel.colon.ramirez@gmail.com>
2024-01-23 16:24:46 -0600

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Angel Colon-Ramirez on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
