8ad1736c05d40981279217dca131d712a543d980
Angel Colon-Ramirez <angel.colon.ramirez@gmail.com>
2024-01-25 13:17:32 -0600

    Adds TabBar()

---
+++ Recipes/Views/Main/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  Recipes
+//
+//  Created by Angel Colon-Ramirez on 1/25/24.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
